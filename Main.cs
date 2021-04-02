1b40ca6df3b0bbe49540ba3ecc73965ad71ec48c
SDraw <sdraw@protonmail.com>
2021-04-02 14:08:31 +0300

    Source code

---
+++ Main.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace ml_var
+{
+    public class VertexAnimationRemover : MelonLoader.MelonMod
+    {
+        [Harmony.HarmonyPatch(typeof(SkinnedMeshRenderer), "BakeMesh")]
+        class Patch_SkinnedMeshRenderer_BakeMesh
+        {
+            static void Postfix(ref SkinnedMeshRenderer __instance)
+            {
+                __instance?.sharedMesh?.ClearBlendShapes();
+            }
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ml_var")]
+[assembly: AssemblyProduct("ml_var")]
+[assembly: ComVisible(false)]
+[assembly: Guid("21380a08-97d8-4fdd-afcd-8cbc9e02f300")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: MelonLoader.MelonInfo(typeof(ml_var.VertexAnimationRemover), "Vertex Animation Remover", "1.0.0", "SDraw", "https://github.com/SDraw/ml_var")]
+[assembly: MelonLoader.MelonGame(null, null)]
